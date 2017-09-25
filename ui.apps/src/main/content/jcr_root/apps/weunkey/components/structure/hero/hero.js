8c4bceb1c1305d6f3211b6359c014bc146ca498b
Kevin Nennig <knennig213@gmail.com>
2017-09-24 23:58:26 -0700

    hero Update

--- ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/hero/hero.js
+++ ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/hero/hero.js
@@ -1,11 +1,30 @@
 "use strict";
 use(function() {
 	var curPage = currentPage.getTitle();
-	var title = properties.get("jcr:title", currentPage.getTitle() || currentPage.getName());
-	var subtitle = properties.get("subtitle");
+	var title = properties.get("heroTitle", currentPage.getTitle() || currentPage.getName());
+	var subtitle = properties.get("heroSubtitle");
+	var bTitle = properties.get("buttonTitle");
+	var bLink = properties.get("buttonLink");
+	
+    //Check for file reference from the DAM
+    var image = properties.get("fileReference", String.class);
+    if(image == "undefined"){
+    	//Check for file upload
+    	var res = resource.getChild("file");
+    	if(res != null){
+    		image = res.getPath();
+    	}
+    } 
+	
+    if(image != "undefined"){
+    	style = "background-image:url(" + image + ");";
+    }
 
 	return {
 		title: title,
-		subtitle: subtitle
+		subtitle: subtitle,
+		buttonTitle: bTitle,
+		buttonLink: bLink,
+		style: style
     }
 });
