daeb28b8b189b8956a6e7b4908ec4454ed1620f1
Kevin Nennig <knennig213@gmail.com>
2017-09-24 22:59:47 -0700

    Hero dialog update

--- ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/hero/hero.js
+++ ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/hero/hero.js
@@ -1,1 +1,11 @@
+"use strict";
+use(function() {
+	var curPage = currentPage.getTitle();
+	var title = properties.get("jcr:title", currentPage.getTitle() || currentPage.getName());
+	var subtitle = properties.get("subtitle");
 
+	return {
+		title: title,
+		subtitle: subtitle
+    }
+});
