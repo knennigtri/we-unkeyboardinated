d34992ec91295ce6782690f119de95223a477b18
Kevin Nennig <knennig213@gmail.com>
2017-09-24 22:28:27 -0700

    Completed footer policy

--- ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/footer/footer.js
+++ ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/footer/footer.js
@@ -1,9 +1,57 @@
 "use strict";
 use(function() {
+	var lTitle = currentStyle.get("leftTitle", "Placeholder");
+	var lPagePaths = currentStyle.get("leftPagePaths");
+	
+	var mTitle = currentStyle.get("middleTitle", "Placeholder");
+	var mPagePaths = currentStyle.get("middlePagePaths");
+	
+	var rTitle = currentStyle.get("rightTitle", "Placeholder");
+	var rText = currentStyle.get("rightText", "Your Text Here");
+	
+	var pManager = resolver.adaptTo(com.day.cq.wcm.api.PageManager);
+	
+	//Create an array of left column links
+	var leftPages = [];
+	if(lPagePaths != null){
+		if(lPagePaths instanceof Array){
+			lPagePaths.forEach( function (arrayItem)
+					{
+					    var pageObject = pManager.getPage(arrayItem);
+					    log.info("LeftPage: " + pageObject.getTitle());
+					    leftPages.push(pageObject);
+					});
+		}else{
+			var pageObject = pManager.getPage(lPagePaths);
+		    log.info("singleLeftPage: " + pageObject.getTitle());
+		    leftPages.push(pageObject);
+		}
+	}
+	
+	//Create an array of middle column links
+	var middlePages = [];
+	if(mPagePaths != null){
+		if(mPagePaths instanceof Array){
+			mPagePaths.forEach( function (arrayItem)
+					{
+					    var pageObject = pManager.getPage(arrayItem);
+					    log.info("MiddlePage: " + pageObject.getTitle());
+					    middlePages.push(pageObject);
+					});
+		}else{
+			var pageObject = pManager.getPage(mPagePaths);
+		    log.info("singleMiddlePage: " + pageObject.getTitle());
+		    middlePages.push(pageObject);
+		}
+	}
+	
 	return {
-        col1title: currentStyle.get("col1title", "Placeholder"),
-        col2title: currentStyle.get("col2title", "Placeholder"),
-        col3title: currentStyle.get("col3title", "Placeholder"),
-        col3text: currentStyle.get("col3text", "Your Text here"),
+		leftTitle: lTitle,
+		leftPagePaths: leftPages,
+		middleTitle: mTitle,
+		middlePagePaths: middlePages,
+		rightTitle: rTitle,
+		rightText: rText
+
     }
 });
--- ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/header/header.js
+++ ui.apps/src/main/content/jcr_root/apps/weunkey/components/structure/header/header.js
@@ -3,7 +3,7 @@ use(function () {
     var items = [];
     var root = currentPage.getAbsoluteParent(3);
     var it = root.listChildren(new Packages.com.day.cq.wcm.api.PageFilter());
-    log.info("Template: " + root.getTitle());
+    log.info("Header Nav Root: " + root.getTitle());
 
     while (it.hasNext()) {
         var page = it.next();
